e40495602c7941b3a27c0bc0e1aee3d9307e72cc
hashbk <littlefishyu2008@gmail.com>
2026-08-05 17:41:55 +0800

    fix: change update check API from POST to GET with query params

--- src/services/rustdesk-console/system.ts
+++ src/services/rustdesk-console/system.ts
@@ -16,8 +16,8 @@ export async function getLicenseStatus(options?: { [key: string]: any }) {
 
 export async function checkUpdate(params: API.UpdateCheckParams, options?: { [key: string]: any }) {
   return request<API.UpdateCheckResult>('/api/update-check', {
-    method: 'POST',
-    data: params,
+    method: 'GET',
+    params,
     ...(options || {}),
   });
 }
