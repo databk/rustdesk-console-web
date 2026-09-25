3d9e5043ac746677f288f81e8380a8cb2bb960a4
hashbk <littlefishyu2008@gmail.com>
2026-09-25 13:41:49 +0000

    fix(dashboard): add height 100%

--- src/pages/dashboard/DashboardContent.tsx
+++ src/pages/dashboard/DashboardContent.tsx
@@ -339,7 +339,7 @@ const Dashboard: React.FC<DashboardProps> = ({
       <Col xs={24} lg={6} style={{ display: 'flex' }}>
         <Flex vertical gap={16} style={{ width: '100%' }}>
           <Card
-            style={{ width: '100%' }}
+            style={{ width: '100%',height: '100%' }}
             size="small"
             title={
               <FormattedMessage
@@ -400,7 +400,7 @@ const Dashboard: React.FC<DashboardProps> = ({
             </Row>
           </Card>
           <Card
-            style={{ width: '100%' }}
+            style={{ width: '100%',height: '100%' }}
             size="small"
             title={
               <FormattedMessage
